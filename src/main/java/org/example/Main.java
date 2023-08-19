1a84e3f1397f023b9b853800649fe9b9e7caa719
Neko Null <jerrylu.ms@outlook.com>
2023-08-19 23:20:44 +0800

    feat: 支持 1.3 版本

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,39 @@
 package org.example;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Main {
     public static void main(String[] args) {
-        // paddleocr_json 的可执行文件所在路径
-        String exePath = "path/to/executable";
-
         // 可选的配置项
         Map<String, Object> arguments = new HashMap<>();
         // arguments.put("use_angle_cls", true);
 
-        // 初始化 OCR
+        // 初始化 OCR：使用本地进程或者套接字服务器
+        // 本地进程: new Ocr(new File(exePath), arguments)
+        String exePath = "path_to_exe"; // paddleocr_json 的可执行文件所在路径
         try (Ocr ocr = new Ocr(new File(exePath), arguments)) {
+//        使用套接字服务器
+//        try (Ocr ocr = new Ocr("localhost", 23333, arguments)) {
 
-            // 对一张图片进行 OCR
-            String imgPath = "path/to/img";
+
+
+            // 对一张图片进行 OCR（使用路径）
+            String imgPath = "path_to_img";
             OcrResponse resp = ocr.runOcr(new File(imgPath));
 
+            // 或者使用图片数据（二进制或 base64）
+//            byte[] fileBytes = Files.readAllBytes(Paths.get(imgPath));
+//            OcrResponse resp = ocr.runOcrOnImgBytes(fileBytes);
+
+//            OcrResponse resp = ocr.runOcrOnImgBase64("base64img");
+
             // 或者直接识别剪贴板中的图片
-            // OcrResponse resp = ocr.runOcrOnClipboard();
+//             OcrResponse resp = ocr.runOcrOnClipboard();
 
             // 读取结果
             if (resp.code == OcrCode.OK) {
--- src/main/java/org/example/Ocr.java
+++ src/main/java/org/example/Ocr.java
@@ -1,11 +1,12 @@
 package org.example;
 
 import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
 
 import java.io.*;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import java.io.FilterWriter;
@@ -85,6 +86,11 @@ class EscapedWriter extends FilterWriter {
     }
 }
 
+enum OcrMode {
+    LOCAL_PROCESS,  // 本地进程
+    SOCKET_SERVER  // 套接字服务器
+}
+
 class OcrCode {
     public static final int OK = 100;
     public static final int NO_TEXT = 101;
@@ -131,14 +137,55 @@ class OcrResponse {
 }
 
 public class Ocr implements AutoCloseable {
-    Process p;
+    // 公共
+    Gson gson;
+    boolean ocrReady = false;
+    Map<String, Object> arguments;
     BufferedReader reader;
     BufferedWriter writer;
-    Gson gson;
+    OcrMode mode;
 
-    boolean ocrReady = false;
+    // 本地进程模式
+    Process process;
+    File exePath;
 
+
+    // 套接字服务器模式
+    String serverAddr;
+    int serverPort;
+    Socket clientSocket;
+    boolean isLoopback = false;
+
+    /**
+     * 使用套接字模式初始化
+     * @param serverAddr
+     * @param serverPort
+     * @param arguments
+     * @throws IOException
+     */
+    public Ocr(String serverAddr, int serverPort, Map<String, Object> arguments) throws IOException {
+        this.mode = OcrMode.SOCKET_SERVER;
+        this.arguments = arguments;
+        this.serverAddr = serverAddr;
+        this.serverPort = serverPort;
+        checkIfLoopback();
+        initOcr();
+    }
+
+    /**
+     * 使用本地进程模式初始化
+     * @param exePath
+     * @param arguments
+     * @throws IOException
+     */
     public Ocr(File exePath, Map<String, Object> arguments) throws IOException {
+        this.mode = OcrMode.LOCAL_PROCESS;
+        this.arguments = arguments;
+        this.exePath = exePath;
+        initOcr();
+    }
+
+    private void initOcr() throws IOException {
         gson = new Gson();
 
         String commands = "";
@@ -154,54 +201,104 @@ public class Ocr implements AutoCloseable {
             }
         }
 
-        if (!commands.contains("use_debug")) {
-            commands += ' ' + "--use_debug=0";
-        }
-
         if (!StandardCharsets.US_ASCII.newEncoder().canEncode(commands)) {
             throw new IllegalArgumentException("参数不能含有非 ASCII 字符");
         }
 
-        System.out.println("当前参数：" + commands);
+        System.out.println("当前参数：" + (commands.isEmpty() ? "空": commands));
 
-        File workingDir = exePath.getParentFile();
-        ProcessBuilder pb = new ProcessBuilder(exePath.toString(), commands);
-        pb.directory(workingDir);
-        pb.redirectErrorStream(true);
-        p = pb.start();
 
-        InputStream stdout = p.getInputStream();
-        OutputStream stdin = p.getOutputStream();
-        reader = new BufferedReader(new InputStreamReader(stdout, StandardCharsets.UTF_8));
-        writer = new BufferedWriter(new OutputStreamWriter(stdin, StandardCharsets.UTF_8));
+        switch (this.mode) {
+            case LOCAL_PROCESS: {
+                File workingDir = exePath.getParentFile();
+                ProcessBuilder pb = new ProcessBuilder(exePath.toString(), commands);
+                pb.directory(workingDir);
+                pb.redirectErrorStream(true);
+                process = pb.start();
 
-        String line = "";
-        ocrReady = false;
-        while (!ocrReady) {
-            line = reader.readLine();
-//            System.out.println(line);
-            if (line.contains("OCR init completed")) {
+                InputStream stdout = process.getInputStream();
+                OutputStream stdin = process.getOutputStream();
+                reader = new BufferedReader(new InputStreamReader(stdout, StandardCharsets.UTF_8));
+                writer = new BufferedWriter(new OutputStreamWriter(stdin, StandardCharsets.UTF_8));
+                String line = "";
+                ocrReady = false;
+                while (!ocrReady) {
+                    line = reader.readLine();
+                    if (line.contains("OCR init completed")) {
+                        ocrReady = true;
+                    }
+                }
+                System.out.println("初始化OCR成功");
+                break;
+            }
+            case SOCKET_SERVER: {
+                clientSocket = new Socket(serverAddr, serverPort);
+                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+                writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
                 ocrReady = true;
+                System.out.println("已连接到OCR套接字服务器，假设服务器已初始化成功");
+                break;
             }
         }
 
-        System.out.println("初始化OCR成功");
+
     }
 
+    /**
+     * 使用图片路径进行 OCR
+     * @param imgFile
+     * @return
+     * @throws IOException
+     */
     public OcrResponse runOcr(File imgFile) throws IOException {
-        return this.runOcrOnPath(imgFile.toString());
-    }
-
-    public OcrResponse runOcrOnClipboard() throws IOException {
-        return this.runOcrOnPath("clipboard");
-    }
-
-    private OcrResponse runOcrOnPath(String path) throws IOException {
-        if (!p.isAlive()) {
-            throw new RuntimeException("OCR进程已经退出");
+        if (mode == OcrMode.SOCKET_SERVER && !isLoopback) {
+            System.out.println("套接字模式下服务器不在本地，发送路径可能失败");
         }
         Map<String, String> reqJson = new HashMap<>();
-        reqJson.put("image_dir", path);
+        reqJson.put("image_path", imgFile.toString());
+        return this.sendJsonToOcr(reqJson);
+    }
+
+    /**
+     * 使用剪贴板中图片进行 OCR
+     * @return
+     * @throws IOException
+     */
+    public OcrResponse runOcrOnClipboard() throws IOException {
+        if (mode == OcrMode.SOCKET_SERVER && !isLoopback) {
+            System.out.println("套接字模式下服务器不在本地，发送剪贴板可能失败");
+        }
+        Map<String, String> reqJson = new HashMap<>();
+        reqJson.put("image_path", "clipboard");
+        return this.sendJsonToOcr(reqJson);
+    }
+
+    /**
+     * 使用 Base64 编码的图片进行 OCR
+     * @param base64str
+     * @return
+     * @throws IOException
+     */
+    public OcrResponse runOcrOnImgBase64(String base64str) throws IOException {
+        Map<String, String> reqJson = new HashMap<>();
+        reqJson.put("image_base64", base64str);
+        return this.sendJsonToOcr(reqJson);
+    }
+
+    /**
+     * 使用图片 Byte 数组进行 OCR
+     * @param fileBytes
+     * @return
+     * @throws IOException
+     */
+    public OcrResponse runOcrOnImgBytes(byte[] fileBytes) throws IOException {
+        return this.runOcrOnImgBase64(Base64.getEncoder().encodeToString(fileBytes));
+    }
+
+    private OcrResponse sendJsonToOcr(Map<String, String> reqJson) throws IOException {
+        if (!isAlive()) {
+            throw new RuntimeException("OCR进程已经退出或连接已断开");
+        }
         StringWriter sw = new StringWriter();
         EscapedWriter ew = new EscapedWriter(sw);
         gson.toJson(reqJson, ew);
@@ -220,11 +317,51 @@ public class Ocr implements AutoCloseable {
     }
 
 
+    private void checkIfLoopback() {
+        if (this.mode != OcrMode.SOCKET_SERVER) return;
+        try {
+            InetAddress address = InetAddress.getByName(serverAddr);
+            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(address);
+            if (networkInterface != null && networkInterface.isLoopback()) {
+                this.isLoopback = true;
+            } else {
+                this.isLoopback = false;
+            }
+        } catch (Exception e) {
+            // 非关键路径
+            System.out.println("套接字模式，未能确认服务端是否在本地");
+        }
+        System.out.println("套接字模式下，服务端在本地：" + isLoopback);
+    }
+
+    private boolean isAlive() {
+        switch (this.mode) {
+            case LOCAL_PROCESS:
+                return process.isAlive();
+            case SOCKET_SERVER:
+                return clientSocket.isConnected();
+        }
+        return false;
+    }
+
 
     @Override
     public void close() {
-        if (p.isAlive()) {
-            p.destroy();
+        if (isAlive()) {
+            switch (this.mode) {
+                case LOCAL_PROCESS: {
+                    process.destroy();
+                    break;
+                }
+                case SOCKET_SERVER: {
+                    try {
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
         }
     }
 
